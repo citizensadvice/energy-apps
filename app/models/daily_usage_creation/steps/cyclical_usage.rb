e55c9effbca6d7a2d1d1043e53302f0222bacadb
davidsauntson <david.sauntson@citizensadvice.org.uk>
2025-01-20 12:27:50 +0000

    feat: cycles and variants step

--- app/models/daily_usage_creation/steps/cyclical_usage.rb
+++ app/models/daily_usage_creation/steps/cyclical_usage.rb
@@ -3,11 +3,36 @@
 module DailyUsageCreation
   module Steps
     class CyclicalUsage < WizardSteps::Step
-      attribute :cycle_quantity
-      attribute :kilowatts
+      attribute :cycles
+      attribute :frequency
+      attribute :wattage
+
+      validates :wattage, presence: { message: "Select an option from the list" }, if: ->(model) { model.appliance.variants? }
+      validates_with Validators::CycleFrequencyValidator
 
       def skipped?
         !@store[:cyclical?]
+      end
+
+      def appliance
+        @appliance ||= ::Appliance.new(data: @store[:added_appliance]["data"])
+      end
+
+      def cycle_frequency_error
+        errors[:cycle_frequency].first
+      end
+
+      def frequency_options
+        [
+          FormOption.new(text: "Day", value: :daily),
+          FormOption.new(text: "Week", value: :weekly)
+        ]
+      end
+
+      def cycle_frequency_field_classes
+        classes = ["cads-form-field"]
+        classes << "cads-form-field--has-error" if cycle_frequency_error.present?
+        classes
       end
     end
   end
