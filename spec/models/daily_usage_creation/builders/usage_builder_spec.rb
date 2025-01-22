9000418a1856cc02484b4fdce08cfb9794786037
davidsauntson <david.sauntson@citizensadvice.org.uk>
2025-01-22 15:46:29 +0000

    test: use doubles for testing usage creator
    The previous testing method did not test the wattage and cycles per day calculations that the builder does and passes to the Usage object.  This change uses stubbing to check all of the params passed to the #new method of the Usage object.

--- spec/models/daily_usage_creation/builders/usage_builder_spec.rb
+++ spec/models/daily_usage_creation/builders/usage_builder_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe(DailyUsageCreation::Builders::UsageBuilder) do
   describe "#build" do
-    subject { described_class.new(store).build }
+    subject(:builder) { described_class.new(store) }
 
     let(:store) do
       WizardSteps::Store.new(
@@ -13,7 +13,7 @@ RSpec.describe(DailyUsageCreation::Builders::UsageBuilder) do
           "added_appliance" => added_appliance,
           "cycles" => "10",
           "frequency" => "weekly",
-          "wattage" => "1000"
+          "wattage" => "200"
         }
       )
     end
@@ -26,28 +26,52 @@ RSpec.describe(DailyUsageCreation::Builders::UsageBuilder) do
           },
           "name" => "TEST - Tumble Dryer (condenser)",
           "category" => "Large appliances",
-          "wattage" => 0,
+          "wattage" => "1000",
           "usageType" => "Cycles",
-          "variantOptions" => variant_options
+          "variantOptions" => variant_options.presence
         }
       }
     end
 
     context "when the appliance is cyclical" do
       let(:cyclical) { true }
+      let(:variant_options) { nil }
+
+      let(:cyclical_usage_double) { instance_double(CyclicalDailyUsage) }
+
+      before do
+        allow(CyclicalDailyUsage).to receive(:new).and_return(cyclical_usage_double)
+        builder.build
+      end
+
+      it "creates the correct type of usage" do
+        expect(CyclicalDailyUsage).to have_received(:new)
+      end
 
       context "when the appliance has no variants" do
-        let(:variant_options) { nil }
-
-        it { is_expected.to be_a CyclicalDailyUsage }
-        its(:label) { is_expected.to eq "TEST - Tumble Dryer (condenser), 10 cycles per week" }
-        its(:details) { is_expected.to eq ["Cycle(s): 10"] }
+        it "passes the correct label, details, wattage and cycle quantity" do
+          expected_params = {
+            label: "TEST - Tumble Dryer (condenser), 10 cycles per week",
+            details: ["Cycle(s): 10"],
+            wattage: "1000", # wattage is taken from the appliance not the chosen input
+            cycle_quantity: 1.4285714285714286 # 10 cycles per week / 7
+          }
+          expect(CyclicalDailyUsage).to have_received(:new).with(expected_params)
+        end
       end
 
       context "when the appliance has variants" do
         let(:variant_options) { { "tableData" => [["Option", "Wattage"], ["Full load", "1000"], ["Partial load", "200"]] } }
 
-        its(:label) { is_expected.to eq "TEST - Tumble Dryer (condenser), Full load, 10 cycles per week" }
+        it "passes the correct label, details, wattage and cycle quantity" do
+          expected_params = {
+            label: "TEST - Tumble Dryer (condenser), Partial load, 10 cycles per week",
+            details: ["Cycle(s): 10", "Partial load"],
+            wattage: "200", # wattage is taken from chosen input
+            cycle_quantity: 1.4285714285714286 # 10 cycles per week / 7
+          }
+          expect(CyclicalDailyUsage).to have_received(:new).with(expected_params)
+        end
       end
     end
   end
