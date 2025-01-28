418c03bb1a3557d481e98176b793ddc5169e54d1
davidsauntson <david.sauntson@citizensadvice.org.uk>
2025-01-28 14:32:00 +0000

    test: spec for last added component

---
+++ spec/components/appliance_calculator/last_added_appliance_component_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ApplianceCalculator::LastAddedApplianceComponent, type: :component do
+  subject { page }
+
+  before do
+    render_inline described_class.new(message)
+  end
+
+  context "when there is a message" do
+    let(:message) { "This is the message" }
+
+    it { is_expected.to have_text "This is the message" }
+    it { is_expected.to have_button aria: { label: "Close last added appliance notice" } }
+  end
+
+  context "when there is no message" do
+    let(:message) { nil }
+
+    it { is_expected.to have_no_css "body" }
+  end
+end
