379fcd215ce6c6a16d89c1ff732fac0c62701bf6
davidsauntson <david.sauntson@citizensadvice.org.uk>
2025-02-04 09:25:40 +0000

    feat: move js modules into a sub folder
    We only want propshaft to transpile the application level js and not the individal modules, so I have moved the modules into their own folder.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Entry point for the build script in your package.json
 import initHeader from '@citizensadvice/design-system/lib/header';
-import initGreedyNav from "./greedy-nav";
-import initSupplierTableButton from "./supplier-table";
+import initGreedyNav from "./modules/greedy-nav";
+import initSupplierTableButton from "./modules/supplier-table";
 
 try {
   // Initialise design-system modules first
