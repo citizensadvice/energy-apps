afdc502dce89ff0b7e1a00570d9b2280d13ffcbd
davidsauntson <david.sauntson@citizensadvice.org.uk>
2025-02-10 12:09:50 +0000

    feat: use qsp to render layout wiht header and footer

--- app/controllers/appliance_calculator/application_controller.rb
+++ app/controllers/appliance_calculator/application_controller.rb
@@ -2,11 +2,21 @@
 
 module ApplianceCalculator
   class ApplicationController < ApplicationController
+    layout :choose_layout
+
     def clear
       session[:unit_rate] = nil
       session[:usages] = nil
 
       redirect_to appliance_calculator_daily_usage_creation_step_path("appliance")
+    end
+
+    def choose_layout
+      if params["no-js"].present?
+        "appliance_calculator_no_js"
+      else
+        "appliance_calculator"
+      end
     end
   end
 end
