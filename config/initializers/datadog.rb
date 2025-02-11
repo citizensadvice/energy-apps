7e6a8b1fdc35f1559354511a5830d9014416efa3
davidsauntson <david.sauntson@citizensadvice.org.uk>
2025-02-11 17:45:06 +0000

    feat: log additional headers

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Share a service name in order to group all integrations
-service_name = ENV.fetch("DD_SERVICE", "energy-csr-table")
+service_name = ENV.fetch("DD_SERVICE", "energy-apps")
 ci_test = ENV.fetch("CI_TEST", false)
 SemanticLogger.application = service_name
 
@@ -9,8 +9,17 @@ SemanticLogger.application = service_name
 # Rails in a terminal
 unless $stdout.tty? || ci_test
   Datadog.configure do |c|
+    traced_headers = {
+      request: %w[
+        Host
+        Referer
+        X-Forwarded-Host
+        X-Forwarded-For
+      ]
+    }
+
     c.service = service_name
-    c.tracing.instrument :rails
+    c.tracing.instrument :rails, headers: traced_headers
     c.tracing.instrument :faraday, describes: /graphql\.contentful\.com/, service_name: "#{service_name}-contentful-graphql"
   end
 end
