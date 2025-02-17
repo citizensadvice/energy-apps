788cf7038638678d616bd38f874500ac711291c8
davidsauntson <david.sauntson@citizensadvice.org.uk>
2025-02-17 14:07:45 +0000

    refactor: commonise element tracking

--- app/javascript/modules/appliance-calculator-analytics.js
+++ app/javascript/modules/appliance-calculator-analytics.js
@@ -1,33 +1,29 @@
 // Events are pushed into the datalayer here
 // Events and values are defined on components directly in the rendered HTML
 
+const trackElement = (el) => {
+    const event = el.getAttribute('data-gtm-event-name');
+    const value = el.getAttribute('data-gtm-value');
+
+    if (event && value) {
+        window.dataLayer.push({
+            [event]: value
+        });
+    }
+}
 
 
 const trackClicks = () => {
     const trackedElements = document.querySelectorAll("[data-gtm-event='click']")
     if (trackedElements) {
-        trackedElements.forEach(el => el.addEventListener('click', () => {
-            const event = el.getAttribute('data-gtm-event-name');
-            const value = el.getAttribute('data-gtm-value');
-
-            window.dataLayer.push({
-                [event]: value
-            });
-        }))
+        trackedElements.forEach(el => el.addEventListener('click', () => { trackElement(el)}))
     }
 }
 
 const trackRenders = () => {
     const trackedElements = document.querySelectorAll("[data-gtm-event='render']")
     if (trackedElements) {
-        trackedElements.forEach(el => {
-            const event = el.getAttribute('data-gtm-event-name');
-            const value = el.getAttribute('data-gtm-value');
-
-            window.dataLayer.push({
-                [event]: value
-            });
-        })
+        trackedElements.forEach(el => trackElement(el));
     }
 }
 
