5ba3709e495f862ac5bb20842b0f32a37f7a2135
davidsauntson <david.sauntson@citizensadvice.org.uk>
2025-04-02 10:45:01 +0100

    feat: add rebel energy going bust notice

---
+++ app/components/content/rebel_energy_bust_banner_component.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Content
+  class RebelEnergyBustBannerComponent < ViewComponent::Base
+    def initialize(supplier:, current_country: nil)
+      @supplier = supplier
+      @current_country = current_country
+    end
+
+    def render?
+      # show on the main table page
+      return true if @supplier.blank?
+
+      # show on the rebel energy details page
+      @supplier.id == "rebel-energy"
+    end
+
+    def link
+      "#{country_prefix}/consumer/energy/energy-supply/problems-with-your-energy-supply/your-energy-supplier-has-gone-bust/"
+    end
+
+    private
+
+    attr_reader :supplier, :current_country
+
+    def country_prefix
+      return if current_country.blank? || current_country == "england"
+
+      "/#{current_country}"
+    end
+  end
+end
