cf59611213ac270cde66e28debb120502a270999
KPobeeNorris <kath.pobee-norris@citizensadvice.org.uk>
2025-08-27 15:28:44 +0100

    Update feature module to handle false values from cdk.json

--- app/lib/feature.rb
+++ app/lib/feature.rb
@@ -5,7 +5,11 @@ require "active_model/type"
 # Tiny class for checking if a feature is enabled
 class Feature
   def self.enabled?(flag_name)
-    # Use ActiveModel's boolean type which recognises the strings 'false'/'0'/'' as false values
+    # Setting "false" in the cdk returns "False", which isn't classed a falsey value by ActiveModel::Type::Boolean,
+    # so we need to explicitly return false in these cases
+    return false if ENV.fetch(flag_name, nil) == "False"
+
+    # Use ActiveModel's boolean type which recognises the strings 'false'/'0' etc as false values
     ActiveModel::Type::Boolean.new.cast(ENV.fetch(flag_name, nil))
   end
 end
