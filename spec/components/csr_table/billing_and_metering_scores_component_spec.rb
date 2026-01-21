a5b6c023e8615f47fc7787b7000a2ab1171f34b9
KPobeeNorris <kath.pobee-norris@citizensadvice.org.uk>
2026-01-21 09:04:06 +0000

    Add billing and metering component specs

---
+++ spec/components/csr_table/billing_and_metering_scores_component_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe CsrTable::BillingAndMeteringScoresComponent, type: :component do
+  subject { page }
+
+  let(:supplier) { build(:supplier, :ranked) }
+
+  before do
+    render_inline described_class.new(supplier)
+  end
+
+  it { is_expected.to have_text "Billing and metering" }
+
+  it { is_expected.to have_text "Bills accuracy (smart meters)" }
+  it { is_expected.to have_text "98.3%" }
+  it { is_expected.to have_text "Bills accuracy (traditional meters)" }
+  it { is_expected.to have_text "82.9%" }
+  it { is_expected.to have_text "Smart meters working correctly" }
+  it { is_expected.to have_text "91.7%" }
+
+  context "when there is no supplier" do
+    let(:supplier) { nil }
+
+    it { is_expected.to have_no_css "body" }
+  end
+end
