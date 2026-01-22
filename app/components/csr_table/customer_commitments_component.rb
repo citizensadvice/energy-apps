383a12f763109c8c062a0b44535d8b7a96b411e1
KPobeeNorris <kath.pobee-norris@citizensadvice.org.uk>
2026-01-22 09:45:47 +0000

    chore/Add Customer commitments data
    As part of the CSR table changes in March the Customer commitments data
    is being moved into it's own section, this is existing data that's just
    being split out from where it currently renders.  The only other change
    is that if the supplier isn't part of either scheme we'll render "None"
    rather than leave the cell empty

---
+++ app/components/csr_table/customer_commitments_component.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module CsrTable
+  class CustomerCommitmentsComponent < ViewComponent::Base
+    attr_reader :supplier, :renderer
+
+    def initialize(supplier)
+      super()
+      @supplier = supplier
+      @renderer = Renderers::RichTextRenderer.new
+    end
+
+    def render?
+      supplier.present?
+    end
+
+    def descriptions
+      [
+        {
+          term: content_tag(:p, "Customer commitments"),
+          description: guarantee_list_render
+        }
+      ]
+    end
+
+    def guarantee_list_render
+      supplier.guarantee_list ? renderer.render_without_breaks(supplier.guarantee_list) : content_tag(:p, "None")
+    end
+  end
+end
