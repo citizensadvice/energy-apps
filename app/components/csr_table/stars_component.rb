c7b0d693527d4588e3db3bc40facc4ecae372a63
KPobeeNorris <kath.pobee-norris@citizensadvice.org.uk>
2026-03-23 13:05:12 +0000

    Update star component logic
    As we've now got two half star svgs we need to know when to render each
    of them.  Therefore we're going to pass in a new `half_star` parameter
    and use that, combined with the existing `highlight_star` value to
    calculate the number of half stars and which half star svg to render

--- app/components/csr_table/stars_component.rb
+++ app/components/csr_table/stars_component.rb
@@ -6,10 +6,11 @@ module CsrTable
 
     attr_reader :score
 
-    def initialize(score:, highlight_stars: false)
+    def initialize(score:, highlight_stars: false, half_stars: false)
       super()
       @highlight_stars = highlight_stars
       @score = score
+      @half_stars = half_stars
     end
 
     def star_classes
@@ -25,18 +26,22 @@ module CsrTable
       empty_stars = MAX_SCORE - full_stars - half_stars
 
       full_stars.times { star_list << :full }
-      half_stars.times { star_list << :half }
+      half_stars.times { star_list << half_star_type }
       empty_stars.times { star_list << :empty }
 
       star_list
     end
 
     def half_stars
-      return 0 unless @highlight_stars
+      return 0 unless @half_stars
 
       score_decimal = @score.to_s.split(".").last
 
       score_decimal.to_i < 5 ? 0 : 1
+    end
+
+    def half_star_type
+      @highlight_stars ? :highlighted_half : :half
     end
 
     def render_star(star)
@@ -45,6 +50,8 @@ module CsrTable
         render CsrTable::Star::FullComponent.new
       when :half
         render CsrTable::Star::HalfComponent.new
+      when :highlighted_half
+        render CsrTable::Star::HighlightedHalfComponent.new
       else
         render CsrTable::Star::EmptyComponent.new
       end
